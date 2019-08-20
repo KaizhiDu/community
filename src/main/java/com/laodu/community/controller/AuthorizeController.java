f71817d498bfb74573d929606da189cd4b4b0cd7
laodu <529443744@qq.com>
2019-08-19 23:13:31 -0700

    add the git login and config properties

--- src/main/java/com/laodu/community/controller/AuthorizeController.java
+++ src/main/java/com/laodu/community/controller/AuthorizeController.java
@@ -1,4 +1,51 @@
 package com.laodu.community.controller;
 
+import com.laodu.community.dto.AccessTokenDTO;
+import com.laodu.community.dto.GithubUser;
+import com.laodu.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.rediect.url}")
+    private String rediectUrl;
+
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam("code") String code,
+                           @RequestParam("state") String state,
+                           HttpServletRequest req
+                           ) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRediect_url(rediectUrl);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            req.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
+
+    }
 }
